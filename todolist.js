04f4c4fe3f162b1e5f33d899a06aace699d484d8
Alex Law <achlawuk@gmail.com>
2017-09-11 11:59:29 +0100

    List with dividers

--- todolist.js
+++ todolist.js
@@ -4,13 +4,16 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
   if(input === "list") {
+  	console.log("====================")
     list.forEach(function(todo, index) {
       console.log(index + ": " + todo);
     });
+  	console.log("====================")
   } else if(input === "new") {
     var newList = prompt("Enter your todo here.");
     list.push(newList);
   }
   input = prompt("What would you like to do?")
 }
- console.log("You quit the app, goodbye.")
+
+console.log("You quit the app, goodbye.")
