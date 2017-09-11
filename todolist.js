471c602f4c9e39a280ea5bfe59d79eba09ec7ba6
Alex Law <achlawuk@gmail.com>
2017-09-11 11:03:53 +0100

    new todo

--- todolist.js
+++ todolist.js
@@ -3,5 +3,9 @@ var list = ["Play Dark Souls The Board Game"];
 var input = prompt("Add your todo by typing here.")
 
 if(input === "list") {
-	console.log(list);
+  console.log(list);
+} else if(input === "new") {
+  var newList = prompt("Add another todo?");
+
+  list.push(newList);
 }
